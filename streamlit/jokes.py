8d5436f1c2dea544f87bc46b68688a3550d01551
Carlos Elias Perez Pizarro <c.elias.pp@gmail.com>
2024-10-08 20:44:14 -0300

    titulo

--- streamlit/jokes.py
+++ streamlit/jokes.py
@@ -109,7 +109,7 @@ def generate_altair_bar_chart(df_common_words):
 
 # Función principal de la app
 def main():
-    st.title("Análisis de chistes por festival, humorista y década")
+    st.title("6 decadas de humor en el Festival Internacional de Viña del Mar")
 
     # Inicializar el estado de session_state
     initialize_session_state()
